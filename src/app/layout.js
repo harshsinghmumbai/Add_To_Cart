38f894612a3f28adfc871488671786883aa97a4e
Harsh-Singh09 <harshrsingh552005@gmail.com>
2024-06-11 21:27:31 +0530

    styling card items is ready with fully responsiveness

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
           <Header />
           {children}
         </main>
+        <Toaster />
       </body>
     </html>
   );
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,60 @@
+"use client";
+import { useState } from "react";
+import Card_Data from "@/components/Card_Data";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+
 export default function Home() {
+  const [data, setdata] = useState(Card_Data);
+
+  const sendData = (items) => {
+    console.log(items);
+  };
   return (
     <>
-      <div className="">hii</div>
+      <div className="sm:grid sm:grid-cols-2 sm:gap-2 lg:grid lg:grid-cols-3 lg:gap-4 xl:grid xl:grid-cols-4 xl:gap-3">
+        {data.map((items, id) => {
+          const { imgdata, dish, address, price, rating } = items;
+          return (
+            <div className="w-[300px] rounded-md border m-auto mt-3" key={id}>
+              <img
+                src={imgdata}
+                alt="Laptop"
+                className="h-[200px] w-full rounded-md object-cover"
+              />
+              <div className="p-4">
+                <h1 className="text-lg font-semibold capitalize">{dish}</h1>
+                <p className="mt-1 text-base text-gray-600">{address}</p>
+                <p className="mt-1 flex justify-between mb-3">
+                  <span className="text-base text-blue-500 font-semibold">
+                    {price}
+                  </span>
+                  <span className="text-base text-blue-500 font-semibold">
+                    {rating} ⭐
+                  </span>
+                </p>
+                <Button
+                  variant="outline"
+                  onClick={() => {
+                    toast(`${dish}`, {
+                      description: "Item add to Your Cart",
+                      className:
+                        "group-[.toaster]:bg-green-100 group-[.toaster]:border group-[.toaster]:border-green-800",
+                      cancel: {
+                        label: "Cancel",
+                        onClick: () => console.log("Undo"),
+                      },
+                    });
+                    return sendData(items);
+                  }}
+                >
+                  Add To Card
+                </Button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
